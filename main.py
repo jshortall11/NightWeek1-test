09a6c8af2721736fbe8a7d8174aeefb38f52caca
Shortall <jshortall@student.bridgew.edu>
2022-09-12 20:32:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+your_name = input("What is your name")
+your_major = input("What are you majoring in")
+message1= ''''This String cant be stopped 
+It will take over the world insert evil laugh'''
+print("wow", your_name, "is a very cool and neat name")
+print("The comp151 class will help you achieve your",your_major)
+print(message1)
+comment_to_user = f"wow {your_name} comp151 will really help you with your {your_major}"
+print(comment_to_user)
